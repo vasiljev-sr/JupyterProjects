33c6d7e9e4c1d05c23407fd8baf654418f11fb85
Ravil <ravil.vasilev@yandex.ru>
2020-12-21 11:25:13 +0300

    Добавил функцию активности мыши, теперь скрины прекращаются, если мышь стоит на месте больше заданного времни

--- venv/Keyboard.py
+++ venv/Keyboard.py
@@ -1,15 +1,49 @@
-import sys
+import time
+from datetime import datetime
+import json
+import threading
 
-sys.path.append('..')
-import keyboard
+import pyautogui
 
-
-def print_pressed_keys(e):
-    line = ', '.join(str(code) for code in keyboard._pressed_events)
-    # '\r' and end='' overwrites the previous line.
-    # ' '*40 prints 40 spaces at the end to ensure the previous line is cleared.
-    print('\r' + line + ' ' * 40, end='')
+with open("parametrs.json", "r") as reader:
+    data = json.load(reader)
 
 
-keyboard.hook(print_pressed_keys)
-keyboard.wait()
+def timer():
+    get_time = int(time.time())
+    return get_time
+
+
+NOW = timer()
+SCREENSHOT_TIME = timer()
+
+
+def mouse_is_active():
+    global NOW
+    s = {'pos': pyautogui.position(), 'timer': timer()}
+    while True:
+        NOW = timer()
+        if s['pos'] != pyautogui.position():  # and (timer() - s['timer']) > data["sleep time"] :
+            s = {'pos': pyautogui.position(), 'timer': timer()}
+
+        if NOW - s['timer'] > 10:
+            event.clear()
+        else:
+            event.set()
+
+
+def start():
+    global NOW
+    global SCREENSHOT_TIME
+    while True:
+        if (NOW - SCREENSHOT_TIME) > 5 and event.is_set():
+            image = pyautogui.screenshot(data['path'] + str(datetime.now()) + '.png')
+            SCREENSHOT_TIME = timer()
+        NOW = timer()
+
+
+event = threading.Event()
+t1 = threading.Thread(target=mouse_is_active)
+t2 = threading.Thread(target=start)
+t1.start()
+t2.start()
--- venv/Main.py
+++ venv/Main.py
@@ -1,34 +1,46 @@
 import time
 from datetime import datetime
 import json
-
 import pyautogui
 
 with open("parametrs.json", "r") as reader:
     data = json.load(reader)
 
+
 def timer():
     get_time = int(time.time())
-    return  get_time
+    return get_time
+
+
 NOW = timer()
 SCREENSHOT_TIME = timer()
 
 
 def mouse_is_active():
+    global NOW
+    s = {'pos': pyautogui.position(), 'timer': timer()}
     while True:
-        s = {'pos': pyautogui.position(), 'timer': timer()}
-        if s['pos'] == pyautogui.position(): #and (timer() - s['timer']) > data["sleep time"] :
-
-            return False
+        NOW = timer()
+        if s['pos'] != pyautogui.position():
+            s = {'pos': pyautogui.position(), 'timer': timer()}
+        if NOW - s['timer'] > data["sleep time"]:
+            event.clear()
         else:
-            return True
-while True:
-    if (NOW - SCREENSHOT_TIME) > 5:
-        image = pyautogui.screenshot(data['path'] + str(datetime.now()) + '.png')
-        SCREENSHOT_TIME = timer()
-    NOW = timer()
+            event.set()
 
 
+def start():
+    global NOW
+    global SCREENSHOT_TIME
+    while True:
+        if (NOW - SCREENSHOT_TIME) > data["shot rate"] and event.is_set():
+            image = pyautogui.screenshot(data['path'] + str(datetime.now()) + '.png')
+            SCREENSHOT_TIME = timer()
+        NOW = timer()
 
 
-
+event = threading.Event()
+t1 = threading.Thread(target=mouse_is_active)
+t2 = threading.Thread(target=start)
+t1.start()
+t2.start()
